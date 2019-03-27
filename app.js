9abee4e688a64d8fae12d187e003104c63a444d3
weishaodaren <king.zjw@qq.com>
2019-03-27 20:58:05 +0800

    de

--- app.js
+++ app.js
@@ -9,8 +9,26 @@ app.listen(6605,()=>{
     console.log('服务器启动，监听端口6605...')
 });
 
+//跨域问题  引入
+app.use(cors({
+    origin:["http://127.0.0.1:8080","http://localhost:8080",
+    "http://127.0.0.1:6606","http://localhost:6606",
+    "http://127.0.0.1:3000","http://localhost:3000"],
+    credentials:true
+  }));
+  // 借助cors跨域 下载express-session 配置
+  const session =require("express-session");
+  app.use(session({
+      secret:"128位随机字符",//安全字符串
+      resave:false,        //每次请求是否都更新数据
+      saveUninitialized:true,//初始化保存数据
+      cookie:{
+        maxAge:1000*60*60*8
+      }
+  }));
+  
 // 中间件
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 //托管静态文件
 app.use(express.static("public"));
